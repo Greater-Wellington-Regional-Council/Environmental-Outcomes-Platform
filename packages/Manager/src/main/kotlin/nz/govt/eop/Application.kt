969f1584cae95a0968aecf260800f21adc3acb50
Martin Peak <martin.peak@gw.govt.nz>
2022-11-23 22:11:16 +1300

    whaitua and groundwater tasks

--- packages/Manager/src/main/kotlin/nz/govt/eop/Application.kt
+++ packages/Manager/src/main/kotlin/nz/govt/eop/Application.kt
@@ -6,18 +6,16 @@ import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.client.RestTemplate
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import org.springframework.web.client.RestTemplate
 
 @EnableScheduling
 @EnableCaching
 @SpringBootApplication
 class Application() {
 
-  @Bean
-  fun restTemplate(): RestTemplate = RestTemplateBuilder()
-        .build()
+  @Bean fun restTemplate(): RestTemplate = RestTemplateBuilder().build()
 
   @Bean
   fun corsConfigurer(): WebMvcConfigurer {
