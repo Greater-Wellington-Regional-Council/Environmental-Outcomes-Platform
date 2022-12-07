16faa1d22519ecea9e59930fdb7e3fac939f43cc
Steve Mosley <steve@starter4ten.com>
2022-12-08 11:49:59 +1300

    Attempt allowing CORS headers for EOP hosts

--- packages/Manager/src/main/kotlin/nz/govt/eop/Application.kt
+++ packages/Manager/src/main/kotlin/nz/govt/eop/Application.kt
@@ -6,23 +6,12 @@ import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.web.client.RestTemplate
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @EnableCaching
 @SpringBootApplication
 class Application() {
 
   @Bean fun restTemplate(): RestTemplate = RestTemplateBuilder().build()
-
-  @Bean
-  fun corsConfigurer(): WebMvcConfigurer {
-    return object : WebMvcConfigurer {
-      override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**").allowedOriginPatterns("http://localhost:[*]")
-      }
-    }
-  }
 }
 
 fun main(args: Array<String>) {
