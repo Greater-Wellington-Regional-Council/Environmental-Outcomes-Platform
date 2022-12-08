74d2b8c62342d15ee88bb285f5cf791b8f32a017
Steve Mosley <steve@starter4ten.com>
2022-12-08 13:53:44 +1300

    Adding catchmanagement unit name to GeoJson for surface water

--- packages/Manager/src/main/kotlin/nz/govt/eop/geo/GeoJsonController.kt
+++ packages/Manager/src/main/kotlin/nz/govt/eop/geo/GeoJsonController.kt
@@ -77,6 +77,7 @@ class GeoJsonController(val context: DSLContext) {
                 CATCHMENTS.ID.`as`("id"),
                 CATCHMENTS.GEOM.`as`("geometry"),
                 ALLOCATION_AMOUNTS.ALLOCATION_AMOUNT,
+                ALLOCATION_AMOUNTS.CATCHMENT_MANAGEMENT_UNIT,
                 ALLOCATION_AMOUNTS.AREA_DESCRIPTION,
                 ALLOCATION_AMOUNTS.ALLOCATION_AMOUNT_UNIT)
             .from(CATCHMENTS)
@@ -98,6 +99,7 @@ class GeoJsonController(val context: DSLContext) {
                 CATCHMENTS.ID.`as`("id"),
                 CATCHMENTS.GEOM.`as`("geometry"),
                 ALLOCATION_AMOUNTS.ALLOCATION_AMOUNT,
+                ALLOCATION_AMOUNTS.CATCHMENT_MANAGEMENT_UNIT,
                 ALLOCATION_AMOUNTS.AREA_DESCRIPTION,
                 ALLOCATION_AMOUNTS.ALLOCATION_AMOUNT_UNIT)
             .from(CATCHMENTS)
