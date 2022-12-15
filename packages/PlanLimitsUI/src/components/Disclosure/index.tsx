e92417116da5a36fc9330513cc1cb667e882e6ec
Vim <vim@noodle.io>
2022-12-16 10:45:55 +1300

    Update disclaimer content with draft copy

--- packages/PlanLimitsUI/src/components/Disclosure/index.tsx
+++ packages/PlanLimitsUI/src/components/Disclosure/index.tsx
@@ -18,23 +18,59 @@ export default function Disclosure() {
 
       {/* Full-screen container to center the panel */}
       <div className="fixed inset-0 flex items-center justify-center">
-        <Dialog.Panel className="mx-auto max-w-xl rounded bg-white p-6">
+        <Dialog.Panel className="mx-auto max-w-2xl rounded bg-white p-6">
           <Dialog.Title className="text-xl mb-4">
             Greater Wellington Freshwater Plan Limits Viewer
           </Dialog.Title>
 
           <p className="mb-2 uppercase">Conditions of use</p>
           <p className="mb-4">
-            Greater Wellington Regional Council's mapping service provides many
-            layers of regional data and information, as compiled by Greater
-            Wellington Regional Council and/or supplied by various government
-            and utility agencies. All reasonable efforts are made to ensure the
-            currency and accuracy of the information contained in this viewer.
-            Whilst due care has been taken in providing this website, all
-            information should be considered as being illustrative and
-            indicative only. Your use of information from this website is
-            entirely at your own risk.
+            Your use of this website is subject to the following conditions and
+            all applicable laws. By accessing and browsing this website you
+            accept, without limitation or qualifications, that you have read,
+            understood and agree to these terms below:
           </p>
+          <div className="mb-4 h-80 overflow-scroll border rounded p-2">
+            <ol className="list-decimal list-inside">
+              <li className="mb-2 text-sm">
+                Wellington Regional Council aims for information in this website
+                to be accurate, however we do not warranty the information is
+                error-free or up-to-date. We do not accept any responsibility of
+                liability for any action taken or omission made as a result of
+                reliance placed on the Council because of having read part or
+                all of this website or any linked sites or for any error or
+                inadequacy in the information contain in this website.
+              </li>
+              <li className="mb-2 text-sm">
+                Where appropriate, external links have been provided for your
+                convenience. The Council does not accept any responsibility for
+                the content or condition of any external links on this site.
+              </li>
+              <li className="mb-2 text-sm">
+                Information contained in the website cannot be used in legal
+                disputes.
+              </li>
+              <li className="mb-2 text-sm">
+                Whilst due care had been taken in providing this website, all
+                information should be considered as being illustrative and
+                indicative only. Your use of information from this website is
+                entirely at your own risk and we recommend that information be
+                interdependently verified before any action is taken in reliance
+                on it.
+              </li>
+              <li className="mb-2 text-sm">
+                The Council may modify, alter or otherwise update the terms
+                applicable to this website from time to time. You agree to check
+                these terms and conditions regularly and be bound by such terms
+                as are in effect at the time you access this website.
+              </li>
+              <li className="mb-2 text-sm">
+                Your use of this website and the terms shall be governed by the
+                laws of New Zealand without reference to principles of conflict
+                laws
+              </li>
+            </ol>
+          </div>
           <div className="flex justify-end">
             <Button
               onClick={() => setIsOpen(false)}
@@ -42,7 +78,6 @@ export default function Disclosure() {
             ></Button>
           </div>
         </Dialog.Panel>
-         
       </div>
     </Dialog>
   );
