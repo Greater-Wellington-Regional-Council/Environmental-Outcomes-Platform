d078e1bb14adedef3e1d25186a0d7923f091e4cb
Martin Peak <martin.peak@gw.govt.nz>
2022-12-20 12:45:08 +1300

    Add more descriptive names for management points.

---
+++ packages/Manager/src/main/resources/db/migration/V0005__update_min_flow_data.sql
@@ -0,0 +1,132 @@
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Kopuaranga River at Palmers Bridge'
+WHERE
+  id = 3;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Waipoua River at Mikimiki Bridge'
+WHERE
+  id = 4;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Waingawa River at Kaituna'
+WHERE
+  id = 5;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Parkvale Stream at Renalls Weir'
+WHERE
+  id = 6;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Waiohine River at Gorge'
+WHERE
+  id = 7;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Papawai Stream at Fabians Road'
+WHERE
+  id = 8;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Ruamāhanga River at Wardells'
+WHERE
+  id = 9;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Otukura Stream at Weir'
+WHERE
+  id = 10;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Tauherenikau River at Gorge'
+WHERE
+  id = 11;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Ruamāhanga River at Waihenga Bridge'
+WHERE
+  id = 12;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Hutt River at Kaitoke'
+WHERE
+  id = 13;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Hutt River at Birchville'
+WHERE
+  id = 14;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Wainuiomata River at Manuka Track'
+WHERE
+  id = 15;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Wainuiomata River Leonard Wood Park'
+WHERE
+  id = 16;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Orongorongo River at Truss Bridge'
+WHERE
+  id = 17;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Waitohu Stream at Water Supply Intake'
+WHERE
+  id = 18;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Ōtaki Gorge at Pukehinau'
+WHERE
+  id = 19;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Mangaone Stream at Ratanui'
+WHERE
+  id = 20;
+
+UPDATE
+  minimum_flow_limits
+SET
+  plan_management_point_name = 'Waikanae River at Water Treatment Plant'
+WHERE
+  id = 21;
