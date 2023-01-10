b9fc4b0f707a30c2d484504b8dc3b527b3d1fac1
Steve Mosley <steve@starter4ten.com>
2023-01-11 11:22:04 +1300

    Update to latest spring 2.7 branch

--- packages/Manager/build.gradle.kts
+++ packages/Manager/build.gradle.kts
@@ -2,10 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jooq.meta.jaxb.ForcedType
 
 plugins {
-  id("org.springframework.boot") version "2.7.5"
-  id("io.spring.dependency-management") version "1.0.11.RELEASE"
-  kotlin("jvm") version "1.7.10"
-  kotlin("plugin.spring") version "1.7.10"
+  id("org.springframework.boot") version "2.7.7"
+  id("io.spring.dependency-management") version "1.0.15.RELEASE"
+  kotlin("jvm") version "1.7.22"
+  kotlin("plugin.spring") version "1.7.22"
   id("com.diffplug.spotless") version "6.11.0"
   id("org.flywaydb.flyway") version "9.1.6"
   id("nu.studer.jooq") version "8.0"
@@ -46,6 +46,8 @@ dependencies {
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("io.kotest:kotest-assertions-core:5.5.4")
 }
+
+tasks.getByName<Jar>("jar") { enabled = false }
 
 tasks.withType<KotlinCompile> {
   kotlinOptions {
