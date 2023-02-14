3a06f0b6395fcbdb01a01a99dc4f6ffa54f02489
Vim <vim@noodle.io>
2023-02-15 08:45:19 +1300

    Log AuthenticationPrincipal for testing

--- packages/IngestAPI/src/main/kotlin/nz/govt/eop/ingestapi/Controller.kt
+++ packages/IngestAPI/src/main/kotlin/nz/govt/eop/ingestapi/Controller.kt
@@ -2,7 +2,10 @@ package nz.govt.eop.ingestapi
 
 import java.time.Instant
 import java.util.UUID
+import mu.KotlinLogging
 import org.springframework.http.MediaType
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,9 +13,13 @@ data class IngestResponse(val receivedAt: String, val ingestID: String)
 
 @RestController
 class Controller(val producer: Producer) {
+
+  private val logger = KotlinLogging.logger {}
+
   @GetMapping("/allocations", produces = [MediaType.APPLICATION_JSON_VALUE])
-  fun allocations(): IngestResponse {
+  fun allocations(@AuthenticationPrincipal apiUser: UserDetails): IngestResponse {
     // producer.produce("The message")
+    logger.info { "Ingest called by ${apiUser.username}" }
     return IngestResponse(
         receivedAt = Instant.now().toString(),
         ingestID = UUID.randomUUID().toString(),
