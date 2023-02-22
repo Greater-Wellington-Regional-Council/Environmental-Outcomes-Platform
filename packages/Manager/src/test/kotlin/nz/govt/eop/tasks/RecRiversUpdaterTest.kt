d436c08e87f020108ae0d48a69a43a5cd86a93b9
Steve Mosley <steve@starter4ten.com>
2023-02-22 13:32:35 +1300

    Modifying the REC at Booths Creek

--- packages/Manager/src/test/kotlin/nz/govt/eop/tasks/RecRiversUpdaterTest.kt
+++ packages/Manager/src/test/kotlin/nz/govt/eop/tasks/RecRiversUpdaterTest.kt
@@ -4,6 +4,7 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import java.time.LocalDateTime
 import nz.govt.eop.si.jooq.tables.RawRecFeaturesRivers.Companion.RAW_REC_FEATURES_RIVERS
+import nz.govt.eop.si.jooq.tables.RecRiversModifications.Companion.REC_RIVERS_MODIFICATIONS
 import nz.govt.eop.si.jooq.tables.Rivers.Companion.RIVERS
 import org.jooq.DSLContext
 import org.jooq.JSONB
@@ -45,6 +46,7 @@ class RecRiversUpdaterTest(
   fun `should insert records when rivers table is empty`() {
     // GIVEN
     context.truncate(RAW_REC_FEATURES_RIVERS).execute()
+    context.truncate(REC_RIVERS_MODIFICATIONS).execute()
     context.truncate(RIVERS).execute()
 
     context
@@ -72,6 +74,7 @@ class RecRiversUpdaterTest(
   fun `should do nothing when last record in rivers table was created after last raw table ingest`() {
     // GIVEN
     context.truncate(RAW_REC_FEATURES_RIVERS).execute()
+    context.truncate(REC_RIVERS_MODIFICATIONS).execute()
     context.truncate(RIVERS).execute()
 
     context
@@ -107,6 +110,7 @@ class RecRiversUpdaterTest(
   fun `should update rivers table last record in rivers table was created before last raw table ingest`() {
     // GIVEN
     context.truncate(RAW_REC_FEATURES_RIVERS).execute()
+    context.truncate(REC_RIVERS_MODIFICATIONS).execute()
     context.truncate(RIVERS).execute()
 
     context
@@ -140,5 +144,55 @@ class RecRiversUpdaterTest(
     record.nzSegment.shouldBe(123456)
     record.streamOrder.shouldBe(3)
     record.isHeadwater.shouldBe(true)
+  }
+
+  @Test
+  fun `should include rows from the rec river modifications table before the raw rec rivers`() {
+    // GIVEN
+    context.truncate(RAW_REC_FEATURES_RIVERS).execute()
+    context.truncate(REC_RIVERS_MODIFICATIONS).execute()
+    context.truncate(RIVERS).execute()
+
+    context
+        .insertInto(
+            REC_RIVERS_MODIFICATIONS,
+            REC_RIVERS_MODIFICATIONS.HYDRO_ID,
+            REC_RIVERS_MODIFICATIONS.IS_HEADWATER,
+            REC_RIVERS_MODIFICATIONS.NEXT_HYDRO_ID,
+            REC_RIVERS_MODIFICATIONS.NZ_SEGMENT,
+            REC_RIVERS_MODIFICATIONS.STREAM_ORDER,
+            REC_RIVERS_MODIFICATIONS.GEOM,
+            REC_RIVERS_MODIFICATIONS.CREATED_AT,
+            REC_RIVERS_MODIFICATIONS.COMMENT)
+        .values(
+            DSL.value(2),
+            DSL.value(false),
+            DSL.value(3),
+            DSL.value(654321),
+            DSL.value(4),
+            DSL.field("ST_GeomFromText('POINT(0 0)')", ByteArray::class.java),
+            DSL.value(LocalDateTime.now()),
+            DSL.value("TEST DATA"))
+        .execute()
+
+    context
+        .insertInto(RAW_REC_FEATURES_RIVERS)
+        .columns(RAW_REC_FEATURES_RIVERS.DATA)
+        .values(JSONB.valueOf(JSONObject(defaultRawData).toString()))
+        .execute()
+
+    // WHEN
+    recRiversUpdater.checkRivers()
+
+    // THEN
+    val first = context.select(DSL.count()).from(RIVERS).first()
+    first["count"].shouldBeInstanceOf<Int>().shouldBe(1)
+
+    val record = context.selectFrom(RIVERS).first()
+    record.hydroId.shouldBe(2)
+    record.nextHydroId.shouldBe(3)
+    record.nzSegment.shouldBe(654321)
+    record.streamOrder.shouldBe(4)
+    record.isHeadwater.shouldBe(false)
   }
 }
