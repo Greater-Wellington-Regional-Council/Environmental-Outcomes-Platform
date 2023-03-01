c6a4bdf9c9457a1ac8514725e35c4904267c03ba
Vim <vim@noodle.io>
2023-03-02 12:39:01 +1300

    Update parent surface water units for the Waiarapa

--- packages/Manager/src/main/resources/db/migration/V0014__add_parent_sw_unit_to_allocations.sql
+++ packages/Manager/src/main/resources/db/migration/V0014__add_parent_sw_unit_to_allocations.sql
@@ -11,3 +11,19 @@ WHERE id IN (
 	ON aa.parent_surfacewatersubunit_id = paa.id
 	WHERE paa.management_unit_type = 'MANAGEMENT_UNIT'
 );
+
+UPDATE  allocation_amounts
+SET parent_surfacewater_unit_id = 16
+WHERE id IN (9, 11);
+
+UPDATE  allocation_amounts
+SET parent_surfacewater_unit_id = 29
+WHERE id IN (
+  1,2,3,4,5,6,8,
+  10,12,13,14,
+  45,46,47,48,
+  54,55,56,58,
+  61,67,68,69,
+  70,73,
+  80
+);
