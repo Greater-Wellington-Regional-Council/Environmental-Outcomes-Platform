f482789105ae149211a0121ef92b285042c4a73b
Vimal Jobanputra <vim@noodle.io>
2023-03-01 14:18:05 +1300

    Update IngestAPI format (#33)

--- packages/IngestAPI/src/main/kotlin/nz/govt/eop/ingestapi/Controller.kt
+++ packages/IngestAPI/src/main/kotlin/nz/govt/eop/ingestapi/Controller.kt
@@ -1,7 +1,6 @@
 package nz.govt.eop.ingestapi
 
 import java.time.Instant
-import java.util.UUID
 import mu.KotlinLogging
 import mu.withLoggingContext
 import org.springframework.http.MediaType
@@ -11,9 +10,14 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
-data class IngestResponse(val ingestId: String, val receivedAt: String, val recordsIngested: Int)
+data class WaterAllocation(val areaId: String, val amount: Int)
 
-data class WaterAllocation(val id: String, val amount: Int)
+data class WaterAllocationsRequestBody(
+    val ingestId: String,
+    val allocations: List<WaterAllocation>
+)
+
+data class IngestResponse(val ingestId: String, val receivedAt: String, val recordsIngested: Int)
 
 @RestController
 class Controller(private val producer: Producer) {
@@ -23,22 +27,21 @@ class Controller(private val producer: Producer) {
   @PostMapping("/water-allocations", produces = [MediaType.APPLICATION_JSON_VALUE])
   fun waterAllocations(
       @AuthenticationPrincipal apiUser: UserDetails,
-      @RequestBody allocations: List<WaterAllocation>
+      @RequestBody requestBody: WaterAllocationsRequestBody
   ): IngestResponse {
 
-    val ingestId = UUID.randomUUID().toString()
     val receivedAt = Instant.now().toString()
 
-    withLoggingContext("ingestId" to ingestId) {
-      logger.info { "Recieved ${allocations.size} allocations" }
-      producer.produce(allocations, ingestId, receivedAt)
-      logger.info { "Ingested ${allocations.size} allocations" }
+    withLoggingContext("ingestId" to requestBody.ingestId) {
+      logger.info { "Recieved ${requestBody.allocations.size} allocations" }
+      producer.produce(requestBody.allocations, requestBody.ingestId, receivedAt)
+      logger.info { "Ingested ${requestBody.allocations.size} allocations" }
     }
 
     return IngestResponse(
-        ingestId = ingestId,
+        ingestId = requestBody.ingestId,
         receivedAt = receivedAt,
-        recordsIngested = allocations.size,
+        recordsIngested = requestBody.allocations.size,
     )
   }
 }
