caa3749a5b8efe96abf377e4b8a725c653c3dea6
Vim <vim@noodle.io>
2023-03-09 16:52:02 +1300

    Configure consumer error handling behaviour

--- packages/Manager/src/main/kotlin/nz/govt/eop/consumers/WaterAllocationConsumer.kt
+++ packages/Manager/src/main/kotlin/nz/govt/eop/consumers/WaterAllocationConsumer.kt
@@ -8,20 +8,30 @@ import nz.govt.eop.si.jooq.tables.WaterAllocations.Companion.WATER_ALLOCATIONS
 import org.jooq.*
 import org.jooq.impl.DSL.*
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer
+import org.springframework.kafka.listener.DefaultErrorHandler
 import org.springframework.stereotype.Component
+import org.springframework.util.backoff.ExponentialBackOff
 
 const val WATER_ALLOCATION_TOPIC_NAME = "water.allocation"
 
 @Component
-class WaterAllocationConsumer(@Autowired val context: DSLContext) {
+class WaterAllocationConsumer(
+    @Autowired val context: DSLContext,
+    private val kafkaTemplate: KafkaTemplate<String, WaterAllocationMessage>
+) {
 
   private val logger = KotlinLogging.logger {}
 
   @KafkaListener(
-      topics = [WATER_ALLOCATION_TOPIC_NAME], groupId = "nz.govt.eop.consumers.water-allocation")
+      topics = [WATER_ALLOCATION_TOPIC_NAME], id = "nz.govt.eop.consumers.water-allocation")
   fun processMessage(allocation: WaterAllocationMessage) {
     withLoggingContext("ingestId" to allocation.ingestId) {
+      logger.info { "Consuming water allocation for area_id:${allocation.areaId}" }
       context
           .insertInto(WATER_ALLOCATIONS)
           .columns(
@@ -38,5 +48,12 @@ class WaterAllocationConsumer(@Autowired val context: DSLContext) {
           .execute()
       logger.info { "Consumed water allocation for area_id:${allocation.areaId}" }
     }
+  }
+
+  @Bean
+  fun errorHandler(kafkaProperties: KafkaProperties): DefaultErrorHandler = run {
+    val backOff = ExponentialBackOff(1000, 2.0)
+    backOff.maxElapsedTime = 10000
+    DefaultErrorHandler(DeadLetterPublishingRecoverer(kafkaTemplate), backOff)
   }
 }
