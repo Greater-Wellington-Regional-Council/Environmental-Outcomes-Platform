7215f1945cf063be3792236cd44da5487bcfbffc
Steve Mosley <steve@starter4ten.com>
2023-03-30 10:39:57 +1300

    Update root element in config to match package name

--- packages/IngestAPI/src/main/kotlin/nz/govt/eop/ingest/ApplicationConfiguration.kt
+++ packages/IngestAPI/src/main/kotlin/nz/govt/eop/ingest/ApplicationConfiguration.kt
@@ -2,7 +2,7 @@ package nz.govt.eop.ingest
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "ingest-api")
+@ConfigurationProperties(prefix = "ingest")
 data class ApplicationConfiguration(val topicReplicas: Int, val users: List<User>)
 
 data class User(val username: String, val token: String)
