715871fc1d7c913f27bd34269a5158d1d5d3e9a8
Steve Mosley <steve.mosley@gw.govt.nz>
2023-04-14 11:43:32 +1200

    Docs - What is EOP (#66)
    * Getting started page updates
    
    * Update with code review suggestions

--- docs-site/src/pages/index.tsx
+++ docs-site/src/pages/index.tsx
@@ -2,11 +2,15 @@ import * as React from 'react';
 import Layout from '../components/layout';
 import ArticleHeader from '../components/article-header';
 import Prose from '../components/prose';
+import { graphql, PageProps } from 'gatsby';
 
-export default function Index() {
+export default function Index({ data }: PageProps<Queries.MarkdownPageQuery>) {
+  const { markdownRemark } = data;
+  const { html } = markdownRemark!!;
+
   return (
     <Layout currentPath="/">
-      <ArticleHeader title="Getting Started" section="Introduction" />
+      <ArticleHeader title="What is EOP" section="Introduction" />
       <Prose>
         <div>
           <p className="text-center font-bold italic">
@@ -18,50 +22,8 @@ export default function Index() {
             The vision is to have an environment focused platform that has
             outcomes at heart.
           </p>
-          <h2>Overview</h2>
-          <p>
-            EOP is an initiative led by GWRC building on the experience of ECAN
-            to build a set of systems for managing environmental information in
-            such a way to lead to actionable outcomes in a way that can support
-            all councils.
-          </p>
-          <p>
-            <span className="font-bold">TODO</span> Describe what we mean by
-            outcomes
-          </p>
-          <p>
-            <span className="font-bold">TODO</span> Talk about regional sector
-            governance
-          </p>
-          <h2>Roadmap</h2>
-          <p>
-            In these pages we document the architectural vision for EOP. When it
-            comes to building out the platform the intention is to focus on
-            building a MVP slice through the architecture that supports an
-            delivering for an outcome to a set of users and only what is
-            required to deliver that. Then the next outcome we will deliver will
-            build on top of the previous and evolve things as needed as we learn
-            more.
-          </p>
-          <p>
-            We trust the we can evolve the architecture as we go and adjust when
-            new information comes to light.
-          </p>
-          <h2>Open Source</h2>
-          <p>
-            EOP is a collaboration being developed as Open Source Software (OSS
-            - License MIT) making it open and available for all New Zealand
-            regional councils. The regional councils are expected to be the main
-            contributors whether via their own staff or employing a third party
-            vendor to contribute on their behalf.
-          </p>
-          <h2>Hosting</h2>
-          <p>
-            While this is being built as OSS there is a cost to host and
-            maintain an instance of the platform. The platform is built to be
-            multi-tenanted with a small number of instances run to support the
-            councils of New Zealand.
-          </p>
+
+          <div dangerouslySetInnerHTML={{ __html: html!! }} />
         </div>
       </Prose>
     </Layout>
@@ -71,3 +33,11 @@ export default function Index() {
 export function Head() {
   return <title>Environmental Outcomes Platform (EOP)</title>;
 }
+
+export const pageQuery = graphql`
+  query MarkdownPage {
+    markdownRemark(frontmatter: { key: { eq: "index" } }) {
+      html
+    }
+  }
+`;
