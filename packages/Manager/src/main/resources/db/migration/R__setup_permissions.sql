5714473e836c0f2e3f13383ef9bb5856452abc65
Steve Mosley <steve.mosley@gw.govt.nz>
2023-10-24 16:00:39 +1300

    Moving WaterAllocation View to a Materialised View (#125)
    * Moving WaterAllocation View to a Materialised View
    
    * Remove spots
    
    * Updates to allow water_allocation_and_usage_by_area to be refreshed
    
    * Fix up permission issue with materialized view
    
    * Turn effective_daily_consents back into a regular migration so we can ensure it is run before water_allocation_and_usage_by_area
    
    * Add materialized_views_role definition to LocalInfrastructure init script
    
    * Ensure materialized view is refreshed before running tests
    
    * Ensure materialized_views_role has the same permissions as eop_manager_app_user
    
    * Ensure view is materialized after generating test data
    
    * Remove old version of DB config
    
    - Has been superceded by LocalInfra folder
    
    * Limit materialized views role to Read only
    
    ---------
    
    Co-authored-by: Vim <vim@noodle.io>

--- packages/Manager/src/main/resources/db/migration/R__setup_permissions.sql
+++ packages/Manager/src/main/resources/db/migration/R__setup_permissions.sql
@@ -11,6 +11,17 @@ GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO eop_manager_app_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
   GRANT USAGE, SELECT ON SEQUENCES TO eop_manager_app_user;
 
+-- Permissions for the materialized_views_role
+GRANT USAGE ON SCHEMA public TO materialized_views_role;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+  GRANT SELECT ON TABLES TO materialized_views_role;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO materialized_views_role;
+
+GRANT materialized_views_role TO eop_manager_app_user;
+GRANT materialized_views_role TO eop_manager_migrations_user WITH ADMIN OPTION;
+
 -- Permissions for developer users debugging.
 GRANT USAGE ON SCHEMA public TO developers;
 
@@ -23,3 +34,4 @@ GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO developers;
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
   GRANT USAGE, SELECT ON SEQUENCES TO developers;
+
