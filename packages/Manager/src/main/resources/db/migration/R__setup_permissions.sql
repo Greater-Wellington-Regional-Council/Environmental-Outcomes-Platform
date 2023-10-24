d718b089216eaf6f6abd02b5fb9d3af4602648d1
Steve Mosley <steve@starter4ten.com>
2023-10-24 15:52:12 +1300

    Limit materialized views role to Read only

--- packages/Manager/src/main/resources/db/migration/R__setup_permissions.sql
+++ packages/Manager/src/main/resources/db/migration/R__setup_permissions.sql
@@ -12,14 +12,12 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA public
   GRANT USAGE, SELECT ON SEQUENCES TO eop_manager_app_user;
 
 -- Permissions for the materialized_views_role
-GRANT USAGE, CREATE ON SCHEMA public TO materialized_views_role;
+GRANT USAGE ON SCHEMA public TO materialized_views_role;
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
-  GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO materialized_views_role;
+  GRANT SELECT ON TABLES TO materialized_views_role;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO materialized_views_role;
-
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO materialized_views_role;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO materialized_views_role;
 
 GRANT materialized_views_role TO eop_manager_app_user;
 GRANT materialized_views_role TO eop_manager_migrations_user WITH ADMIN OPTION;
