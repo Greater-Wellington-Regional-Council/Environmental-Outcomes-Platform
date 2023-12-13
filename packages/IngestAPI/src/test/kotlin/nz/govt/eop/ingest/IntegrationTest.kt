7b2b23f57e0b754a9b7400f5b8505a5932c58aba
Steve Mosley <steve@starter4ten.com>
2023-12-14 09:41:18 +1300

    Update Ingest API for new message format

--- packages/IngestAPI/src/test/kotlin/nz/govt/eop/ingest/IntegrationTest.kt
+++ packages/IngestAPI/src/test/kotlin/nz/govt/eop/ingest/IntegrationTest.kt
@@ -49,6 +49,63 @@ class IntegrationTest(@Autowired val mvc: MockMvc, @Autowired val broker: Embedd
   }
 
   @Test
+  fun `Should reject null allocationPlan`() {
+    mvc.perform(
+            post("/water-allocations")
+                .with(SecurityMockMvcRequestPostProcessors.httpBasic("gw", "test-api-token"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    """
+                {
+                  "ingestId": "1",
+                  "allocations":
+                    [
+                        { "sourceId": "OrongorongoSW-20540", "consentId":  "20540", "status":  "active", "areaId":  "OrongorongoSW", "allocationPlan": null, "isMetered": true, "allocationDaily": 0.0, "allocationYearly": 0.0, "meters": [] },
+                    ]
+                }
+              """))
+        .andExpect(status().isBadRequest)
+  }
+
+  @Test
+  fun `Should reject null allocationDaily`() {
+    mvc.perform(
+            post("/water-allocations")
+                .with(SecurityMockMvcRequestPostProcessors.httpBasic("gw", "test-api-token"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    """
+                {
+                  "ingestId": "1",
+                  "allocations":
+                    [
+                        { "sourceId": "OrongorongoSW-20540", "consentId":  "20540", "status":  "active", "areaId":  "OrongorongoSW", "allocationPlan": 0.0, "isMetered": true, "allocationDaily": null, "allocationYearly": 0.0, "meters": [] },
+                    ]
+                }
+              """))
+        .andExpect(status().isBadRequest)
+  }
+
+  @Test
+  fun `Should reject null allocationYearly`() {
+    mvc.perform(
+            post("/water-allocations")
+                .with(SecurityMockMvcRequestPostProcessors.httpBasic("gw", "test-api-token"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    """
+                {
+                  "ingestId": "1",
+                  "allocations":
+                    [
+                        { "sourceId": "OrongorongoSW-20540", "consentId":  "20540", "status":  "active", "areaId":  "OrongorongoSW", "allocationPlan": 0.0, "isMetered": true, "allocationDaily": 0.0, "allocationYearly": null, "meters": [] },
+                    ]
+                }
+              """))
+        .andExpect(status().isBadRequest)
+  }
+
+  @Test
   fun `Should post records to Kafka Topic`() {
     val consumer = createKafkaConsumer(broker)
     val testData =
@@ -57,9 +114,9 @@ class IntegrationTest(@Autowired val mvc: MockMvc, @Autowired val broker: Embedd
               "ingestId": "1",
               "allocations":
                 [
-                    { "sourceId": "OrongorongoSW-20540", "consentId":  "20540", "status":  "active", "areaId":  "OrongorongoSW", "allocation": 87.0, "isMetered": true, "meteredAllocationDaily": 0.0, "meteredAllocationYearly": 0.0, "meters": [] },
-                    { "sourceId": "Wairarapa-20544", "consentId":  "20544", "status":  "active", "areaId":  "Wairarapa CoastSW", "allocation": 2.1, "isMetered": false, "meteredAllocationDaily": null, "meteredAllocationYearly": null, "meters": ["S25/5314"] },
-                    { "sourceId": "OtakiSW-20548", "consentId":  "20548", "status":  "inactive", "areaId":  "OtakiSW", "allocation": 1.13, "isMetered": true, "meteredAllocationDaily": 6912.0, "meteredAllocationYearly": 1036800.0, "meters": ["BP34/0044", "BP34/0064", "T26/0871"] }
+                    { "sourceId": "OrongorongoSW-20540", "consentId":  "20540", "status":  "active", "areaId":  "OrongorongoSW", "allocationPlan": 87.0, "isMetered": true, "allocationDaily": 0.0, "allocationYearly": 0.0, "meters": [] },
+                    { "sourceId": "Wairarapa-20544", "consentId":  "20544", "status":  "active", "areaId":  "Wairarapa CoastSW", "allocationPlan": 2.1, "isMetered": false, "allocationDaily": 0.0, "allocationYearly": 0.0, "meters": ["S25/5314"] },
+                    { "sourceId": "OtakiSW-20548", "consentId":  "20548", "status":  "inactive", "areaId":  "OtakiSW", "allocationPlan": 1.13, "isMetered": true, "allocationDaily": 6912.0, "allocationYearly": 1036800.0, "meters": ["BP34/0044", "BP34/0064", "T26/0871"] }
                 ]
             }
         """
