a8e0202d61426ea42379c06c53ce280e760e1408
Steve Mosley <steve@starter4ten.com>
2024-01-09 14:05:57 +1300

    Update Jackson max message size

--- packages/HilltopCrawler/src/main/kotlin/nz/govt/eop/hilltop_crawler/Application.kt
+++ packages/HilltopCrawler/src/main/kotlin/nz/govt/eop/hilltop_crawler/Application.kt
@@ -1,5 +1,6 @@
 package nz.govt.eop.hilltop_crawler
 
+import com.fasterxml.jackson.core.StreamReadConstraints
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
@@ -12,5 +13,9 @@ class Application {}
 
 fun main(args: Array<String>) {
   System.setProperty("com.sun.security.enableAIAcaIssuers", "true")
+
+  StreamReadConstraints.overrideDefaultStreamReadConstraints(
+      StreamReadConstraints.builder().maxStringLength(50_000_000).build())
+
   runApplication<Application>(*args)
 }
