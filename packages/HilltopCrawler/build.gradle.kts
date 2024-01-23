27a211bd1cb3c1c1e3aab91b47e53ac6b1ac3604
Steve Mosley <steve@starter4ten.com>
2024-01-23 21:44:33 +1300

    Update all the other flyway versions as well

--- packages/HilltopCrawler/build.gradle.kts
+++ packages/HilltopCrawler/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
   id("org.springframework.boot") version "3.2.0"
   id("io.spring.dependency-management") version "1.1.4"
   id("com.diffplug.spotless") version "6.23.3"
-  id("org.flywaydb.flyway") version "10.1.0"
+  id("org.flywaydb.flyway") version "10.6.0"
   id("com.adarshr.test-logger") version "4.0.0"
   kotlin("jvm") version "1.9.21"
   kotlin("plugin.spring") version "1.9.21"
@@ -38,8 +38,8 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   implementation("org.springframework.kafka:spring-kafka")
-  implementation("org.flywaydb:flyway-core:10.1.0")
-  implementation("org.flywaydb:flyway-database-postgresql:10.1.0")
+  implementation("org.flywaydb:flyway-core:10.6.0")
+  implementation("org.flywaydb:flyway-database-postgresql:10.6.0")
   implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
