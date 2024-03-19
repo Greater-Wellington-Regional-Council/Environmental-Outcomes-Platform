f2ab25705e06955ea678088f83731cfbb293fe18
wtcarter-gw <wayne.carter@gw.govt.nz>
2024-03-19 20:08:51 +1300

    TEAMU-998 Click and show location in console

--- packages/cccv/src/test/e2e/show-map.spec.ts
+++ packages/cccv/src/test/e2e/show-map.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test'
 test('home page should have the correct title and welcome message', async ({
   page
 }) => {
-  await page.goto('http://localhost:5174/map/@-41,175.35,8z')
+  await page.goto('http://localhost:5173/map/@-41,175.35,8z')
 
   await expect(page.getByTestId('InteractiveMap')).toBeVisible()
 })
