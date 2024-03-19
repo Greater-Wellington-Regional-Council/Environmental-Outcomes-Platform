5dc814622379bf5de3db1e2d29a65b9555e18044
wtcarter-gw <wayne.carter@gw.govt.nz>
2024-03-19 21:59:24 +1300

    TEAMU-998 Add popup to map

--- packages/cccv/start.sh
+++ packages/cccv/start.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+if [ -n "$1" ]; then
+  echo "Checking if anything is running on port $1"
+  lsof -i :$1
+  if [ $? -eq 0 ]; then
+    echo "Something is running on port $1. Kill it? (y/n)"
+    read kill
+    if [ "$kill"=="y" ]; then
+      lsof -i :$1 | awk 'NR!=1 {print $2}' | xargs kill
+    else
+      echo "Ok, quitting.   Nothing done."
+      exit 1
+    fi
+  fi
+fi
+
 npm run dev
+
