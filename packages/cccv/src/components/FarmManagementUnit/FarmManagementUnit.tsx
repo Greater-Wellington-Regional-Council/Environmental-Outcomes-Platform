27b423d220ca3a6fd720c09ba2fc119f7baf8add
wtcarter-gw <wayne.carter@gw.govt.nz>
2024-04-12 19:52:36 +1200

    TEAMU-1099 Panel UX refinement

--- packages/cccv/src/components/FarmManagementUnit/FarmManagementUnit.tsx
+++ packages/cccv/src/components/FarmManagementUnit/FarmManagementUnit.tsx
@@ -2,7 +2,7 @@ import FarmManagementUnitProps from "@src/models/FarmManagementUnit.ts";
 import "./FarmManagementUnit.scss";
 
 const FarmManagementUnit = ( { id, fmuName1, ecoliBase, ecoliObj, byWhen }: FarmManagementUnitProps ) => {
-  return <div className="farmManagementUnit space-y-0 p-6" id={`fmu_${id}`}>
+  return <div className="farmManagementUnit space-y-0 p-6 h-full bg-gray-700" id={`fmu_${id}`}>
     <h2 className={"space-x-0 space-y-0"}>{fmuName1}</h2>
     <h4>Overview</h4>
     <h4>Outcomes</h4>
