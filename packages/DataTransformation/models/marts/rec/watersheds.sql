931d2b96b30e4a98dcfbdb1499f06556fe03a513
Steve Mosley <steve.mosley@gw.govt.nz>
2024-04-23 23:54:30 +1200

    Adding in view for Tile Server app (#272)

--- packages/DataTransformation/models/marts/rec/watersheds.sql
+++ packages/DataTransformation/models/marts/rec/watersheds.sql
@@ -1,11 +1,17 @@
+{{ config(
+    materialized = 'table',
+    indexes=[
+      {'columns': ['hydro_id'], 'unique': True},
+      {'columns': ['geom'], 'type': 'gist'},
+    ]
+) }}
+
 WITH modifications AS (
-
   SELECT * FROM {{ ref('stg_rec_features_watersheds_modifications') }}
-
 ),
 
 raw AS (
-  SELECT * FROM {{ ref('stg_raw_rec_features_watersheds') }}
+  SELECT * FROM {{ ref('stg_raw_rec_features_watersheds') }} WHERE hydro_id NOT IN (SELECT hydro_id FROM modifications)
 ),
 
 combined AS (
@@ -14,5 +20,9 @@ combined AS (
   SELECT * FROM raw
 )
 
-SELECT *
+SELECT
+  hydro_id,
+  nz_segment,
+  geom::GEOMETRY (GEOMETRY, 4326),
+  created_at
 FROM combined
