1611890b40a6e394f3044a116c85658cb0e85c08
swapna josmi sam <swapnasam2008@gmail.com>
2024-04-24 12:08:44 +1200

    dbt chnages

--- packages/DataTransformation/models/marts/planlimits/dbt_surface_water_limits.sql
+++ packages/DataTransformation/models/marts/planlimits/dbt_surface_water_limits.sql
@@ -1,3 +1,7 @@
+{{ config(
+    materialized = 'view'
+) }}
+
 WITH council_plan_documents AS (
 
   SELECT * FROM {{ ref('stg_planlimits_council_plan_documents') }}
@@ -34,44 +38,54 @@ temp_plan_regions AS (
 expanded_surface_water_limits AS (
 
   SELECT
+
     tpr.council_id,
     pr.plan_id,
     pr.id AS plan_region_id,
     JSONB_ARRAY_ELEMENTS(region -> 'surfaceWaterLimits') AS surface_water_limit
+
   FROM
     temp_plan_regions AS tpr
+
   INNER JOIN
-    plan_regions AS pr ON tpr.plan_id = pr.plan_id AND tpr.region ->> 'id' = pr.source_id
+    plan_regions AS pr 
+      ON tpr.plan_id = pr.plan_id AND tpr.region ->> 'id' = pr.source_id
 ),
 
 id_surface_water_limits AS (
 
   SELECT
+
+    ROW_NUMBER() OVER () AS id,
     council_id,
     plan_region_id,
-    surface_water_limit,
-    ROW_NUMBER() OVER () AS id
+    surface_water_limit
   FROM
     expanded_surface_water_limits
 ),
 
 expanded_surface_water_sub_limits AS (
   SELECT
+
     swl.council_id,
     swl.plan_region_id,
     swl.id AS parent_surface_water_limit_id,
     JSONB_ARRAY_ELEMENTS(swl.surface_water_limit -> 'children') AS surface_water_limit
+
   FROM
     id_surface_water_limits AS swl
 ),
 
 id_surface_water_sub_limits AS (
+
   SELECT
+
+    ROW_NUMBER() OVER () + (SELECT MAX(id) FROM id_surface_water_limits) AS id,
     council_id,
     plan_region_id,
     parent_surface_water_limit_id,
-    surface_water_limit,
-    ROW_NUMBER() OVER () + (SELECT MAX(id) FROM id_surface_water_limits) AS id
+    surface_water_limit
+
   FROM
     expanded_surface_water_sub_limits
 ),
@@ -85,16 +99,22 @@ combined_surface_water_limits AS (
     surface_water_limit ->> 'name',
     (surface_water_limit -> 'allocationLimit')::INT AS allocation_limit,
     (
-      SELECT boundary
+      SELECT 
+        boundary
       FROM council_plan_boundaries
+
       WHERE
         council_id = id_surface_water_limits.council_id
         AND surface_water_limit ->> 'boundaryId' = council_plan_boundaries.source_id
     ) AS boundary
+
   FROM
     id_surface_water_limits
+
   UNION ALL
+
   SELECT
+
     id,
     plan_region_id,
     surface_water_limit ->> 'id' AS source_id,
@@ -102,15 +122,19 @@ combined_surface_water_limits AS (
     surface_water_limit ->> 'name',
     (surface_water_limit -> 'allocationLimit')::INT AS allocation_limit,
     (
-      SELECT boundary
+      SELECT 
+        boundary
       FROM council_plan_boundaries
+
       WHERE
         council_id = id_surface_water_sub_limits.council_id
         AND surface_water_limit ->> 'boundaryId' = council_plan_boundaries.source_id
     ) AS boundary
+    
   FROM
     id_surface_water_sub_limits
 )
 
-SELECT *
-FROM combined_surface_water_limits;
+SELECT 
+  *
+FROM combined_surface_water_limits
