782756b80bc7344c79554839f95f2fdd99592cd8
swapna josmi sam <swapnasam2008@gmail.com>
2024-04-24 16:21:30 +1200

    changes for dbt planlimits

---
+++ packages/DataTransformation/models/marts/planlimits/dbt_water_allocations_by_area.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+WITH water_allocations AS (
+
+  SELECT * FROM {{ ref('stg_planlimits_water_allocations') }}
+),
+
+water_allocations_by_area AS (
+
+  SELECT
+
+    area_id,
+    SUM(allocation_plan) AS allocation_amount
+
+  FROM water_allocations
+    WHERE
+        effective_to IS NULL
+    GROUP BY 1
+
+)
+
+SELECT * FROM water_allocations_by_area
---
+++ packages/DataTransformation/models/staging/planlimits/stg_planlimits_water_allocations.sql
@@ -0,0 +1,24 @@
+WITH source AS (
+    
+  SELECT
+
+    id,
+    area_id,
+    allocation_plan,
+    ingest_id,
+    created_at,
+    updated_at,
+    source_id,
+    consent_id,
+    status,
+    is_metered,
+    allocation_daily,
+    allocation_yearly,
+    meters,
+    effective_from,
+    effective_to
+
+  FROM {{ source('public', 'water_allocations') }}
+)
+
+SELECT * FROM source
