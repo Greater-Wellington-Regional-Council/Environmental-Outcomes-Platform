896e14571678a15b7817c8a72ea85270c8cdd25a
wtcarter-gw <wayne.carter@gw.govt.nz>
2024-05-21 16:45:57 +1200

    Fix: a couple of tweaks to the Info panel

--- packages/cccv/src/components/FreshwaterManagementUnit/FreshwaterManagementUnit.tsx
+++ packages/cccv/src/components/FreshwaterManagementUnit/FreshwaterManagementUnit.tsx
@@ -9,7 +9,7 @@ import formatFilename from "@lib/formatAsFilename";
 import dateTimeString from "@lib/dateTimeString";
 import {contaminant, contaminants as fmuContaminants} from "@components/FreshwaterManagementUnit/utils.ts";
 
-export const panelStyle = "p-6 h-full bg-gray-700";
+export const panelStyle = "p-6 h-full bg-gray-700 relative overflow-auto";
 
 const FreshwaterManagementUnit = (details: FmuFullDetails) => {
   if (!details?.freshwaterManagementUnit) {
@@ -60,16 +60,16 @@ const FreshwaterManagementUnit = (details: FmuFullDetails) => {
         </div>
       </div>
 
-      {tangataWhenuaSites?.length && <div className="tangata-whenua text-black">
+      {tangataWhenuaSites?.length ? (<div className="tangata-whenua text-black">
           <p className={"font-italic"}>This area contains sites of significance to Tangata Whenua including:-</p>
         {tangataWhenuaSites && (<span className="tangata-whenua-sites">
             <ul>
               {tangataWhenuaSites?.map((site: { location: string }, index: Key | null | undefined) => <li className="list-disc" key={index}>{site?.location}</li>)}
             </ul>
         </span>)}
-      </div>}
+      </div>) : <div></div>}
 
-      <div className="about-link mt-6">
+      <div className="about-link mt-6 bottom">
         <a href={'#about'} className="text-amber-500">About this information</a>
       </div>
   </div>
