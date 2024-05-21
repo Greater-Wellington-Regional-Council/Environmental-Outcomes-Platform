99c58a784eefa6c13666586c8e26084ab2231dca
wtcarter-gw <wayne.carter@gw.govt.nz>
2024-05-21 12:31:07 +1200

    Fix: error level cccv layout

--- packages/cccv/src/Layout.tsx
+++ packages/cccv/src/Layout.tsx
@@ -1,13 +1,8 @@
 import { Outlet } from 'react-router-dom'
-import ErrorContext from "@components/ErrorContext/ErrorContext.ts";
-import { useContext } from 'react';
 
 export default function Layout() {
-  const error = useContext(ErrorContext)?.error;
-  console.log('error', error)
   return (
     <>
-      {error ? <div id="error-message" className="bg-red-600 w-full text-center">{error.message}</div> : null}
       {/*<Disclaimer />*/}
       <Outlet />
     </>
