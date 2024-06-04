18a813b52967a6ff68a7e292b3f7f5eb5ab5daf1
Steve Mosley <steve@starter4ten.com>
2024-06-04 14:16:52 +1200

    Remove code and tests not needed anymore

--- packages/Manager/src/main/kotlin/nz/govt/eop/si/jooq/ManagementUnitType.kt
+++
@@ -1,6 +0,0 @@
-package nz.govt.eop.si.jooq
-
-enum class ManagementUnitType {
-  MANAGEMENT_UNIT,
-  MANAGEMENT_SUB_UNIT
-}
--- packages/Manager/src/main/kotlin/nz/govt/eop/si/jooq/TakeType.kt
+++
@@ -1,6 +0,0 @@
-package nz.govt.eop.si.jooq
-
-enum class TakeType {
-  SURFACE_WATER,
-  GROUNDWATER
-}
