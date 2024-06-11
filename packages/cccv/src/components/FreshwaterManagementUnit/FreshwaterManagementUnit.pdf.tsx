8f7fa83f6f9740f713ab0673df432937828a64ae
wtcarter-gw <wayne.carter@gw.govt.nz>
2024-06-12 01:23:44 +1200

    fix: remover header border and footer if only one page

--- packages/cccv/src/components/FreshwaterManagementUnit/FreshwaterManagementUnit.pdf.tsx
+++ packages/cccv/src/components/FreshwaterManagementUnit/FreshwaterManagementUnit.pdf.tsx
@@ -71,21 +71,20 @@ export const FreshwaterManagementUnitPDF = (details: FmuFullDetails) => {
 
   return (
     <Document>
-      <Page size="A4" style={tw("p-4 bg-white font-sans flex flex-col justify-between")}>
-        <View style={tw("mb-4")} id={`fmu_${id}`}>
-          <View style={tw("bg-nui p-4 pb-6 text-white flex flex-row justify-between items-start")}>
-            <View style={tw("flex flex-col")}>
-              <Text style={tw("h1 mb-1")}>Freshwater Management Unit</Text>
-              <Text style={tw("h2 mb-6")}>Catchment context, challenges and values (CCCV)</Text>
-              <Text style={tw("body")}>Find information useful for creating a Freshwater Farm Plan, such as contaminant
-                goals, sites of significance, and implementation ideas for your catchment area.</Text>
-            </View>
-            <Image style={[{width: 120, height: 'auto'}, tw("ml-4")]} source={gwrcLogo}/>
+      <Page size="A4" style={tw("bg-white font-sans flex flex-col justify-between")}>
+        <View style={tw("p-4 bg-nui text-white flex flex-row justify-between items-start")}>
+          <View style={tw("flex flex-col")}>
+            <Text style={tw("h1 mb-1")}>Freshwater Management Unit</Text>
+            <Text style={tw("h2 mb-6")}>Catchment context, challenges and values (CCCV)</Text>
+            <Text style={tw("body")}>Find information useful for creating a Freshwater Farm Plan, such as contaminant
+              goals, sites of significance, and implementation ideas for your catchment area.</Text>
           </View>
-          <View style={tw("pt-4")}>
+          <Image style={[{width: 120, height: 'auto'}, tw("ml-4")]} source={gwrcLogo}/>
+        </View>
 
-            <View style={tw("mb-4")}>
-              <Text style={tw("h1")}>{fmuName1}</Text>
+        <View id={`fmu_${id}`}>
+            <View style={tw("p-4 pb-0")}>
+              <Text style={tw("h1 leading-0")}>{fmuName1}</Text>
 
               {catchmentDescription ? (<>
                 <Text style={tw("h2 mb-2")}>Overview</Text>
@@ -94,7 +93,7 @@ export const FreshwaterManagementUnitPDF = (details: FmuFullDetails) => {
               </>) : null}
             </View>
 
-            <View style={tw("mb-6")}>
+            <View style={tw("p-4 pb-0")}>
               <Text style={tw("h2 mb-2")}>Contaminants</Text>
               <Text style={tw("body")}>
                 Freshwater objectives from {fmuName1} Whaitua Implementation Plan (as at August 2018)
@@ -104,7 +103,7 @@ export const FreshwaterManagementUnitPDF = (details: FmuFullDetails) => {
             </View>
 
             {tangataWhenuaSites?.length ? (
-              <View style={tw("mb-4")} wrap={false}>
+              <View style={tw("p-4 pb-0")} wrap={false}>
                 <Text style={tw("h2 mb-2")}>Sites of significance</Text>
                 <Text style={tw("body mb-1")}>
                   This area contains sites of significance to Tangata Whenua.
@@ -114,7 +113,7 @@ export const FreshwaterManagementUnitPDF = (details: FmuFullDetails) => {
               </View>
             ) : null}
 
-            <View style={tw("mb-4")} wrap={false}>
+            <View style={tw("p-4 pb-0")} wrap={false}>
               <Text style={tw("h2 mb-2")}>About this Information</Text>
               <Text style={tw("body")}>
                 The content, data, and information used in this app comes from multiple sources,
@@ -123,13 +122,16 @@ export const FreshwaterManagementUnitPDF = (details: FmuFullDetails) => {
                 2020 (Amended January 2024).
               </Text>
             </View>
-          </View>
         </View>
-        <View style={tw('absolute bottom-0 left-0 right-0 flex flex-row justify-between items-center p-4')} fixed>
-          <Text style={tw('text-xs')}>CCCV details for {fmuName1}</Text>
+
+        <View style={tw('absolute bottom-0 left-0 right-0 flex flex-row justify-between items-center p-4')} fixed >
           <Text
             style={tw('text-xs')}
-            render={({ pageNumber, totalPages }) => `Page ${pageNumber} of ${totalPages}`}
+            render={({ pageNumber }) => pageNumber === 1 ? null : `CCCV details for {fmuName1}`}
+          />
+          <Text
+            style={tw('text-xs')}
+            render={({ pageNumber, totalPages }) => pageNumber === 1 ? null : `Page ${pageNumber} of ${totalPages}`}
           />
         </View>
       </Page>
