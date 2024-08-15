0bd574c343a0515e16591291fa6c907ec64e3485
Wayne Carter <wayne@ednastreet.nz>
2024-08-15 12:45:52 +1200

    tidy: remove storybook builds

--- packages/shared/ui/storybook-static/assets/Header.stories-C0uQe69i.js
+++
@@ -1,7 +0,0 @@
-import{f as o}from"./Button-C2dCn8_r.js";import{H as p}from"./Header-DB78Upg_.js";import"./index-DZLKizrv.js";import"./index-DJO9vBfz.js";const l={title:"Example/Header",component:p,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: 'Jane Doe'
-    }
-  }
-}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var n,c,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const f=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,f as __namedExportsOrder,l as default};
