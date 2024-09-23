5090a8cf8ed74526b720bd0cf2a954b7a3cf2c8a
Wayne Carter <wayne@ednastreet.nz>
2024-09-23 21:29:26 +1200

    Simplify PDF button logic

--- packages/cccv/src/components/FreshwaterManagementUnit/FreshwaterManagementUnit.tsx
+++ packages/cccv/src/components/FreshwaterManagementUnit/FreshwaterManagementUnit.tsx
@@ -1,16 +1,16 @@
-import "./FreshwaterManagementUnit.scss"
-import purify from "dompurify"
-import {Key} from "react"
-import {FmuFullDetailsWithMap} from "@models/FreshwaterManagementUnit.ts"
-import {BlobProviderParams, PDFDownloadLink} from "@react-pdf/renderer"
-import {FreshwaterManagementUnitPDF} from "@components/FreshwaterManagementUnit/FreshwaterManagementUnit.pdf"
-import formatFilename from "@lib/formatAsFilename"
-import dateTimeString from "@lib/dateTimeString"
-import {ContaminantList, contaminants as fmuContaminants} from "@components/FreshwaterManagementUnit/utils.ts"
-import EmailLink from "@components/EmailLink/EmailLink.tsx"
-import {Contaminants} from "@components/Contaminants/Contaminants.tsx"
-import makeSafe from "@lib/makeSafe.ts"
-import {parseHtmlListToArray} from "@lib/parseHtmlListToArray.ts"
+import "./FreshwaterManagementUnit.scss";
+import purify from "dompurify";
+import {Key} from "react";
+import { FmuFullDetailsWithMap } from "@models/FreshwaterManagementUnit.ts";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import { FreshwaterManagementUnitPDF } from "@components/FreshwaterManagementUnit/FreshwaterManagementUnit.pdf";
+import formatFilename from "@lib/formatAsFilename";
+import dateTimeString from "@lib/dateTimeString";
+import { ContaminantList, contaminants as fmuContaminants } from "@components/FreshwaterManagementUnit/utils.ts";
+import EmailLink from "@components/EmailLink/EmailLink.tsx";
+import { Contaminants } from "@components/Contaminants/Contaminants.tsx";
+import makeSafe from "@lib/makeSafe.ts";
+import {parseHtmlListToArray} from "@lib/parseHtmlListToArray.ts";
 
 const FreshwaterManagementUnit = (details: FmuFullDetailsWithMap) => {
 
@@ -36,32 +36,22 @@ const FreshwaterManagementUnit = (details: FmuFullDetailsWithMap) => {
             <h1 className="w-[80%]">{fmuName1 || ""}</h1>
 
             <div className="absolute top-0 right-0 m-6 mt-0">
-                <PDFDownloadLink
-                    document={<FreshwaterManagementUnitPDF {...details} />}
-                    fileName={fileName}
-                >
-                    {({loading}: BlobProviderParams) =>
-                        loading ? (
-                            <button disabled>Loading...</button>
-                        ) : (
-                            <button>Print</button>
-                        )
-                    }
+                <PDFDownloadLink document={<FreshwaterManagementUnitPDF {...details} />} fileName={fileName}>
+                    <button>Print</button>
                 </PDFDownloadLink>
             </div>
 
-            <div className="overview mt-6" data-testid="catchment-desc">
-                <h2>Overview</h2>
-                <div
-                    dangerouslySetInnerHTML={{__html: purify.sanitize(makeSafe(catchmentDescription ?? "<p>No overview available</p>"))}}/>
-            </div>
+      <div className="overview mt-6" data-testid="catchment-desc">
+        <h2>Overview</h2>
+        <div dangerouslySetInnerHTML={{ __html: purify.sanitize(makeSafe(catchmentDescription ?? "<p>No overview available</p>")) }} />
+      </div>
 
             <div className="contaminants mt-6">
                 <h2>Contaminants</h2>
                 <p>Freshwater objectives from {fmuName1 || ""} Whaitua Implementation Plan (as at August 2018)</p>
 
                 <div className="mt-4">
-                    <Contaminants contaminants={contaminants}/>
+                    <Contaminants contaminants={contaminants} />
                 </div>
             </div>
 
@@ -78,17 +68,17 @@ const FreshwaterManagementUnit = (details: FmuFullDetailsWithMap) => {
                 </div>
             ) : <div></div>}
 
-            {implementationIdeas ? (
-                <div className="implementation-ideas mt-6">
-                    <h2>Implementation Ideas</h2>
-                    <div className="implementation-ideas">
-                        <ul className={"mt-2"}>
-                            {parseHtmlListToArray(implementationIdeas)?.map((idea: string, index) => <li
-                                className="list-disc my-0" key={index}>{makeSafe(idea)}</li>)}
-                        </ul>
-                    </div>
-                </div>
-            ) : <div></div>}
+      {implementationIdeas ? (
+        <div className="implementation-ideas mt-6">
+          <h2>Implementation Ideas</h2>
+          <div className="implementation-ideas">
+            <ul className={"mt-2"}>
+              {parseHtmlListToArray(implementationIdeas)?.map((idea: string, index) => <li
+                className="list-disc my-0" key={index}>{makeSafe(idea)}</li>)}
+            </ul>
+          </div>
+        </div>
+      ) : <div></div>}
 
             <div className="about-this-information mt-6">
                 <h3>About this information</h3>
