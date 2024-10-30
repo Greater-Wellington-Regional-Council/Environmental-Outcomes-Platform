6a47f07ca52b588582b90e9986ad8888c33165e5
Wayne Carter <wayne@ednastreet.nz>
2024-10-30 14:42:18 +1300

    Add pieces to front end and format

--- packages/cccv/src/components/FreshwaterManagementUnit/components/TangataWhenuaSites.tsx
+++ packages/cccv/src/components/FreshwaterManagementUnit/components/TangataWhenuaSites.tsx
@@ -1,16 +1,20 @@
-import React, { useState, useEffect, Key } from "react"
+import React, {useState, useEffect, Key} from "react"
 import manaWhenuaSiteService from "@services/ManaWhenuaSiteService/ManaWhenuaSiteService.ts"
 import Tooltip from "@elements/Tooltip/Tooltip.tsx"
-import { Feature, FeatureCollection } from "geojson"
+import {Feature, FeatureCollection} from "geojson"
+import { MapPinIcon } from '@heroicons/react/20/solid'
 
 interface TangataWhenuaSitesProps {
     tangataWhenuaSites: FeatureCollection;
     gotoTangataWhenua: (index: number) => void;
+    culturalOverview?: string | null;
 }
 
-const TangataWhenuaSites: React.FC<TangataWhenuaSitesProps> = ({ tangataWhenuaSites, gotoTangataWhenua }) => {
+const TangataWhenuaSites: React.FC<TangataWhenuaSitesProps> = ({tangataWhenuaSites, gotoTangataWhenua, culturalOverview = null}) => {
     const [siteDescriptions, setSiteDescriptions] = useState<{ [key: string]: string | undefined }>({})
     const [tooltip, setTooltip] = useState<{ description: string | null; x: number; y: number } | null>(null)
+
+    console.log(culturalOverview)
 
     useEffect(() => {
         async function fetchDescription(siteName: string) {
@@ -47,7 +51,7 @@ const TangataWhenuaSites: React.FC<TangataWhenuaSitesProps> = ({ tangataWhenuaSi
         const x = e.clientX - parentRect.left  // Calculate x relative to the parent
         const y = e.clientY - parentRect.top   // Calculate y relative to the parent
 
-        setTooltip({ description: description || "Loading...", x, y })
+        setTooltip({description: description || "Loading...", x, y})
     }
 
     const hideTooltip = () => {
@@ -58,17 +62,23 @@ const TangataWhenuaSites: React.FC<TangataWhenuaSitesProps> = ({ tangataWhenuaSi
         <>
             {tangataWhenuaSites?.features.length ? (
                 <div className="tangata-whenua mt-6" onClick={hideTooltip}>
-                    <h2>Tangata Whenua</h2>
-                    <p>This area contains sites of significance to Tangata Whenua including:</p>
+                    <h2>Cultural Significance of the Catchment</h2>
+                    {culturalOverview}
+                    <h3 className="mt-6">Sites of Significance</h3>
+                    <p className="mt-2">This area contains sites of significance to Tangata Whenua including:</p>
                     <div className="tangata-whenua-sites">
-                        <ul className="mt-2 list-disc cursor-pointer">
+                        <ul className="mt-2 cursor-pointer">
                             {tangataWhenuaSites?.features.map((site: Feature, siteIndex: number) => (
-                                <li className="my-0" key={siteIndex} onClick={() => gotoTangataWhenua(siteIndex)}>
-                                    <span className={"underline decoration-dashed decoration-1 hover:decoration-2"}>{site?.properties?.location}</span>
+                                <li className="list-none" key={siteIndex} onClick={() => gotoTangataWhenua(siteIndex)}>
+                                    <div className="group">
+                                        <MapPinIcon className="h-4 w-4 inline-block mr-2 group-hover:fill-kaitoke" onClick={() => gotoTangataWhenua(siteIndex)}/>
+                                        <span
+                                            className={"underline decoration-dashed decoration-1 group-hover:text-kaitoke"}>{site?.properties?.location}</span>
+                                    </div>
                                     <ul className="flex flex-wrap gap-4 list-none p-0 m-0 mt-4 mb-6">
                                         {site?.properties?.locationValues?.split(',').map((siteName: string, index: Key | null | undefined) => (
                                             <li
-                                                className="list-none ml-0 inset-0 bg-gray-300 indent-0 px-2 hover:underline hover:decoration-dashed"
+                                                className="list-none ml-0 mt-0 inset-0 bg-gray-300 indent-0 px-2 hover:underline hover:decoration-dashed"
                                                 key={index}
                                                 onClick={(e) => showSiteDescription(e, siteDescriptions[siteName])}
                                             >
