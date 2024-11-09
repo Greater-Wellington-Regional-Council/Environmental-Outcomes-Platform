56574227d3c8f5e4ff0c73de4be4b535ebb463ca
Martin Peak <martin.peak@gw.govt.nz>
2024-11-09 13:46:48 +1300

    all kotest to same version

--- packages/Manager/build.gradle.kts
+++ packages/Manager/build.gradle.kts
@@ -67,9 +67,9 @@ dependencies {
   implementation("org.locationtech.jts:jts-core:1.20.0")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
-  testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+  testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
   testImplementation("io.kotest:kotest-assertions-core:5.9.1")
-  testImplementation("io.kotest:kotest-framework-engine:5.8.1")
+  testImplementation("io.kotest:kotest-framework-engine:5.9.1")
   testImplementation("org.springframework.kafka:spring-kafka-test")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.2")
   testImplementation("io.mockk:mockk:1.13.10")
