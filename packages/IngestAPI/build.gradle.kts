3e24dd46937376d5abcd2f9ffafdba1beb926915
Martin Peak <martin.peak@gw.govt.nz>
2024-11-20 13:37:57 +1300

    Update build.gradle.kts

--- packages/IngestAPI/build.gradle.kts
+++ packages/IngestAPI/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
   id("com.diffplug.spotless") version "6.25.0"
   id("com.adarshr.test-logger") version "4.0.0"
   kotlin("jvm") version "2.0.21"
-  kotlin("plugin.spring") version "1.9.21"
-  kotlin("kapt") version "1.9.21"
+  kotlin("plugin.spring") version "2.0.21"
+  kotlin("kapt") version "2.0.21"
 }
 
 group = "nz.govt.eop"
