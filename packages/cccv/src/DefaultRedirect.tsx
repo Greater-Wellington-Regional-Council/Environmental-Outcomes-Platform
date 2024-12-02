15664975054062b02d008eb540d50ee2e7dd13d7
Wayne Carter <wayne@ednastreet.nz>
2024-12-02 14:25:42 +1300

    FE CCCV New sections to PDF

--- packages/cccv/src/DefaultRedirect.tsx
+++ packages/cccv/src/DefaultRedirect.tsx
@@ -4,7 +4,7 @@ import {useEffect} from "react"
 export const DefaultRedirect = () => {
     const navigate = useNavigate()
     useEffect(() => {
-        navigate('map/@-41,175.35,8z', { replace: true })
+        navigate('/map/@-41,175.35,8z', { replace: true })
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
