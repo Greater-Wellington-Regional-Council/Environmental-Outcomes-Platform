17911d41482d5a933c34acc8d91287d174869ec7
Martin Peak <martin.peak@gw.govt.nz>
2024-12-18 18:32:57 +1300

    update jvm too

--- packages/HilltopCrawler/build.gradle.kts
+++ packages/HilltopCrawler/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
   id("com.diffplug.spotless") version "6.23.3"
   id("org.flywaydb.flyway") version "11.1.0"
   id("com.adarshr.test-logger") version "4.0.0"
-  kotlin("jvm") version "1.9.21"
+  kotlin("jvm") version "2.1.0"
   kotlin("plugin.spring") version "2.1.0"
 }
 
