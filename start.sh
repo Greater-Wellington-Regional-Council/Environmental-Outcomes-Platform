7e694e6f181531ab3e11bc996689852b12b7eaa6
Martin Peak <martin.peak@gw.govt.nz>
2025-04-07 16:42:24 +1200

    connect manager to datatransforrmation

--- start.sh
+++ start.sh
@@ -15,4 +15,16 @@ run-package () {
   ./start.sh $2 $3 $4 $5 $6 $7 $8 $9
 }
 
-run-package "$@"
+if [ "$1" == "Manager" ]; then
+  echo "Starting Manager..."
+  run-package Manager "${@:2}" & # Run Manager in the background
+
+  echo "Waiting for 60 seconds before starting DataTransformation..."
+  sleep 60
+
+  echo "Starting DataTransformation..."
+  (cd packages/DataTransformation && ./batect run) # Output logs only to the console
+  wait # Wait for all background processes to complete
+else
+  run-package "$@"
+fi
