9d7722443429a9e42f43b0215bc7231ec3d16b2f
Marco <marco@somar.co.nz>
2025-07-17 18:21:37 +1200

    24738 CCCV - TSC and Eslint

--- packages/cccv/src/components/InteractiveMap/InteractiveMap.stories.tsx
+++ packages/cccv/src/components/InteractiveMap/InteractiveMap.stories.tsx
@@ -1,25 +1,32 @@
-import { Meta, StoryFn } from '@storybook/react-vite'
-import InteractiveMap from "./InteractiveMap.tsx"
-import {InteractiveMapProps} from "@components/InteractiveMap/lib/InteractiveMap"
-import {IMViewLocation} from "@shared/types/global"
-import {MapSnapshotProvider} from "@lib/MapSnapshotContext.tsx"
+import { Meta, StoryFn } from "@storybook/react-vite";
+import InteractiveMap from "./InteractiveMap.tsx";
+import { InteractiveMapProps } from "@components/InteractiveMap/lib/InteractiveMap";
+import { IMViewLocation } from "@shared/types/global";
+import { MapSnapshotProvider } from "@lib/MapSnapshotContext.tsx";
 
 export default {
-    title: 'Components/InteractiveMap',
-    component: InteractiveMap,
-} as Meta
+  title: "Components/InteractiveMap",
+  component: InteractiveMap,
+} as Meta;
 
-const Template: StoryFn<InteractiveMapProps> = (args) => <MapSnapshotProvider><InteractiveMap {...args} /></MapSnapshotProvider>
+const Template: StoryFn<InteractiveMapProps> = (args) => (
+  <MapSnapshotProvider>
+    <InteractiveMap {...args} />
+  </MapSnapshotProvider>
+);
 
-export const Default = Template.bind({})
+export const Default = {
+  render: Template,
 
-Default.args = {
+  args: {
     startLocation: {
-        latitude: -41.2865,
-        longitude: 174.7762,
-        zoom: 10,
+      latitude: -41.2865,
+      longitude: 174.7762,
+      zoom: 10,
     },
-    setLocationInFocus: (location: IMViewLocation) => console.log('Location selected:', location),
+    setLocationInFocus: (location: IMViewLocation) =>
+      console.log("Location selected:", location),
     locationInFocus: null,
     children: null,
-}
+  },
+};
