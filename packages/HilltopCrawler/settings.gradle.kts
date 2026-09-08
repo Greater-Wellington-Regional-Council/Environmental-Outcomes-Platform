bf14a5a909267d180d08c5dea9bdf32e20e298f6
Chris Tombleson <chris@somar.co.nz>
2026-09-08 15:23:35 +1200

    IngestAPI, HilltopCrawler & Manager: fixed broken check

--- packages/HilltopCrawler/settings.gradle.kts
+++ packages/HilltopCrawler/settings.gradle.kts
@@ -1,5 +1,1 @@
 rootProject.name = "HilltopCrawler"
-
-plugins {
-  id("io.github.ben-manes.versions.settings") version "0.57.0"
-}
--- packages/IngestAPI/settings.gradle.kts
+++ packages/IngestAPI/settings.gradle.kts
@@ -1,5 +1,1 @@
 rootProject.name = "IngestAPI"
-
-plugins {
-  id("io.github.ben-manes.versions.settings") version "0.61.0"
-}
